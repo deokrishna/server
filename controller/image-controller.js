5d6935b2b82ab8fbe882d1c8f5ecdad5d59a0cee
krishna <kvatsadeo@gmail.com>
2023-02-20 16:04:14 +0530

    localhost_change

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,8 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+// const url = 'http://localhost:8000';
+const url= "https://server-tan-chi.vercel.app";
 
 
 let gfs, gridfsBucket;
